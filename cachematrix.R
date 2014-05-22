ce5fa28a12c4c1d08292b6c6e12baed3f350a249
Laszlo Vass <laszlo.vass@imind.eu>
2014-05-22 08:39:32 +0200

    Programming assignment 2 solved

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Calculate inverse of a matrix and cache the result.
+## The second call for the same matrix will return the result from the cache.
 
-## Write a short comment describing this function
+## Calculate the inverse of a matrix and store the original 
+## and the inverse in cache variable
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cachedOriginalMatrix <<- x
+  cachedInverseMatrix <<- solve(x)
+  cachedInverseMatrix
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve checks if x invserse is calculated already
+## return the cached matrix, or calculate the inverse of the 
+# new matrix, and return it
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        sameAsCache <- FALSE
+        # check if the dimensions are the same
+        if (sum(dim(x) == dim(cachedOriginalMatrix)) == 2) {
+          # count the elements, that are not equal to the cached matrix
+          if (sum(!(x == cachedOriginalMatrix)) == 0) {
+            sameAsCache <- TRUE
+          }
+        }
+        if (sameAsCache) {
+          # Return cached matrix
+          cachedInverseMatrix
+        } else {
+          # Calculate end return
+          makeCacheMatrix(x)
+        }
 }
